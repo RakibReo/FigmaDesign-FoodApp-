d8e0e8d5730f366b43d0da03aa84fe8ffca02589
SuRakib <rakibul15-10976@diu.edu.bd>
2023-03-02 22:47:17 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/foodapp/Onboarding1Activity.java
@@ -0,0 +1,24 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Onboarding1Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding1);
+
+
+
+
+
+
+
+    }
+}
